4649fc2956e111b9def7aa9e7bb5f6b085c4feaf
PawelPaziewski <lrpapawel@gmail.com>
2021-04-12 17:00:00 +0200

    Dodanie HibernateUtil
    (cherry picked from commit e814c153b6e721f07141563ff68e33754d01c7dd)

---
+++ src/main/java/HibernateUtil.java
@@ -0,0 +1,20 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    private static final Configuration configuration = new Configuration().configure("persistence.cfg.xml");
+
+    static {
+        try {
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
